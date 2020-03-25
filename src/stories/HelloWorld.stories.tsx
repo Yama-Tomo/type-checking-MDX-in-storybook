a2e0ebe9cc0f31a4b24f58fc2867ad18fa8421d9
Yamatomo <mazda13bturbo@gmail.com>
2020-03-26 07:48:54 +0900

    feat: add story

---
+++ src/stories/HelloWorld.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from '~/components/HelloWorld';
+
+export const Default = () => <HelloWorld />;
+export const small = () => <HelloWorld size="small" />;
+export const medium = () => <HelloWorld size="medium" />;
+export const large = () => <HelloWorld size="large" />;
+
+export const colorRed = () => <HelloWorld color="red" />;
+colorRed.story = {
+  name: 'color:#ff0000',
+};
+
+export default { title: 'example/HelloWorld-CSF' };
