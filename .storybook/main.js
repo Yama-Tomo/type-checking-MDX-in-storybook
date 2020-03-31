9d1304a19713ae0dd8fd633dca335be5e3c99ce2
Yamatomo <mazda13bturbo@gmail.com>
2020-03-31 12:23:54 +0900

    feat: move eslint to behind the storybook source-loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ const path = require('path')
 const displayErrorOnDevPlugin = require('./webpack/display_error_on_dev_plugin')
 const typeCheckingMdx = require('./webpack/type_checking_mdx')
 
-process.env.EXTEND_ESLINT = 'false'
-
 module.exports = {
   stories: ['../src/stories/**/*.stories.(tsx|mdx)'],
   addons: [
@@ -19,6 +17,19 @@ module.exports = {
     const isDev = configType === 'DEVELOPMENT'
 
     config.resolve.alias['~'] = path.resolve('./src')
+
+    const eslintRule = config.module.rules.find(
+      (rule) =>
+        rule.test && typeof rule.test.test === 'function' && rule.test.test('.jsx') &&
+        rule.enforce === 'pre' &&
+        Array.isArray(rule.use) && rule.use.find((use) => use.loader && use.loader.includes('eslint-loader'))
+    )
+
+    if (eslintRule) {
+      // exclude .storybook dir  
+      eslintRule.include = eslintRule.include.filter(includePath => !includePath.includes('.storybook'))
+      config.module.rules = [...config.module.rules.filter((rule) => rule !== eslintRule), eslintRule]
+    }
 
     typeCheckingMdx(config, { delay: isDev ? 1500 : 5000, isDev })
 
