4e8f01ddfc6c5ea7d39fc8b448b1c6fd530c7e0a
Yamatomo <mazda13bturbo@gmail.com>
2021-03-03 15:46:35 +0900

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
