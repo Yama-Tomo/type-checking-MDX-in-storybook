e5cef3a1878fb4a3bdf2ffa57e2ebe7dc6ba44da
Yama-Tomo <yam4.tomo@gmail.com>
2022-03-03 18:56:34 +0900

    chore: remove unnecessary settings

--- config-overrides.js
+++ config-overrides.js
@@ -9,23 +9,7 @@ module.exports = {
   },
   jest: function (config) {
     config.moduleNameMapper['^~/(.*)$'] = '<rootDir>/src/$1';
-    config.moduleFileExtensions = [...config.moduleFileExtensions, 'd.ts'];
-    config.transform = {
-      '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
-      ...config.transform,
-    };
 
     return config;
-  },
-  paths: (paths, env) => {
-    const overridePaths = { ...paths };
-
-    if (env === 'test') {
-      const pathsConfigPath = path.resolve('node_modules/react-scripts/config/paths.js');
-      // override paths in memory
-      require.cache[require.resolve(pathsConfigPath)].exports = overridePaths;
-    }
-
-    return overridePaths;
   },
 };
